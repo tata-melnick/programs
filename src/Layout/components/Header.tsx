d5613b3c82bc99180c05217ab84ef3277a964a3d
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-14 15:59:19 +0300

    Fix [Header]

--- src/Layout/components/Header.tsx
+++ src/Layout/components/Header.tsx
@@ -8,14 +8,14 @@ const Header: React.FC = () => {
         <div className={styles.header}>
             <div className={styles.logo}>ЛОГОТИП</div>
             <div className={styles.img}>
-                <Button className={styles.btnImg}>
+                <div className={styles.btnImg}>
                     <img  src="/public/notification.png"
                               alt="Notification"/>
-                </Button>
-                <Button className={styles.btnImg}>
+                </div>
+                <div className={styles.btnImg}>
                     <img  src="/public/unknownUser.png"
                               alt="Notification"/>
-                </Button>
+                </div>
             </div>
         </div>
     )
