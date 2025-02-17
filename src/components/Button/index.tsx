6e7fb4545a277074fbefe8c38e27f05708f65f3f
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-17 13:53:13 +0300

    Add component Button and styles [Button]

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import cn from "classnames"
+import styles from "./button.module.css"
+
+interface IButtonProps {
+    type?: "filled" | "outline"
+    className?: string
+    children?: React.ReactNode
+    onClick(): void
+}
+
+const Button: React.FC<IButtonProps> = ({
+    children,
+    onClick,
+    className,
+    type = "text",
+}) => {
+    return (
+        <button
+            onClick={onClick}
+            className={cn(
+                styles.btn,
+                type === "filled" && styles.btnFilled,
+                type === "outline" && styles.btnOutline,
+                className
+            )}
+        >
+            {children && children}
+        </button>
+    )
+}
+
+export default Button
