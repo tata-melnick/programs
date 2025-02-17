bc7ef66fedad21ac55845aaafa33307941b11dc2
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-17 10:51:35 +0300

    Add component Tabs and styles [Button]

---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import styles from "./tabs.module.css";
+import Tab from "./Tab";
+import MainList from "../MainList";
+
+enum TabList {
+    All = "all",
+    My = "my",
+}
+
+const Tabs: React.FC = () => {
+    const [activeTab, setActiveTab] = useState(TabList.All);
+
+    const list = {
+        [TabList.All]: { label: "Все программы", content: <MainList/>},
+        [TabList.My]: { label: "Мои программы", content: <div>my</div>},
+    }
+
+    return (
+        <>
+            <div className={styles.labelTab}>
+                {Object.keys(list).map((tab: TabList) => (
+                    <Tab
+                        label={list[tab].label}
+                        key={tab}
+                        onClick={() => setActiveTab(tab)}
+                        active={tab === activeTab}
+                    />
+                ))}
+            </div>
+            {list[activeTab].content}
+        </>
+    )
+}
+
+export default Tabs;
