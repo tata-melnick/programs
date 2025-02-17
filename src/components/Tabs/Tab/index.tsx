538a084a1954466c26d7cb41612fd27625482d8f
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-17 10:52:04 +0300

    Add component Tab and styles [Tab]

---
+++ src/components/Tabs/Tab/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./tab.module.css"
+
+type TabProps = {
+    label: string;
+    active: boolean;
+    onClick(): void;
+}
+
+const Tab: React.FC<TabProps> = ({label, onClick, active}) => {
+    const activeStyle = active ? {
+        fontWeight: "500",
+    } : {};
+
+    return (
+        <div
+            className={styles.tab}
+            onClick={onClick}
+            style={activeStyle}
+        >
+            {label}
+        </div>
+
+    )
+}
+
+export default Tab;
