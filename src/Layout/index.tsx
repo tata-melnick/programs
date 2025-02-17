b9593ca58955dd940fba7ff3f8d7b852ee92198e
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-17 11:02:46 +0300

    Refactoring component Layout [Layout]

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import styles from "./layout.module.css";
 import Header from "./components/Header";
 import  Footer from "./components/Footer";
-import Main from "../components/Main";
+import Tabs from "../components/Tabs";
+import Menu from "../components/Menu";
 
 const Layout: React.FC = () => {
 
     return (
         <div className={styles.layout}>
             <Header />
-            <Main />
+            <div className={styles.main}>
+                <Menu/>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Иванов Иван</h1>
+                    <Tabs />
+                </div>
+            </div>
             <Footer />
         </div>
     )
