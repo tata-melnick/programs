8dacb8347a846a2a5377d143aa7b4e59a79e5984
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-17 10:53:03 +0300

    Renamed type [types.ts]

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
-export type CardProgram = {
+export type ProgramType = {
     id: string;
     pictures: string;
     title: string;
     deadline: string;
 }
 
-export type ListCards = Array<CardProgram>;
+export type ListCards = Array<ProgramType>;
