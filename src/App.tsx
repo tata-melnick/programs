231703ff28a806dab290640667611c02d89fe3cc
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-18 12:52:33 +0300

    Fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react"
 import { Route, Routes } from "react-router-dom"
-import "./App.css"
 import Layout from "./Layout"
 import { ListCardsContext } from "./context"
 import { ProgramList, ProgramType } from "./types"
 import PROGRAMS from "./mock/programs.json"
+import Programs from "./components/Programs"
 
 const App: React.FC = () => {
     const [list, setList] = useState<ProgramList>(PROGRAMS)
@@ -17,7 +17,14 @@ const App: React.FC = () => {
             value={{ list, myList, setList, addMyProgram }}
         >
             <Routes>
-                <Route path="/" element={<Layout />} />
+                <Route path="/" element={<Layout />}>
+                    <Route path="/" element={<Programs />} />
+                    <Route path="/profile" element={<div>Профиль</div>} />
+                    <Route
+                        path="/certificate"
+                        element={<div>Сертификаты</div>}
+                    />
+                </Route>
             </Routes>
         </ListCardsContext.Provider>
     )
--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,8 +1,8 @@
 import React from "react"
+import { Outlet } from "react-router-dom"
 import styles from "./layout.module.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Tabs from "../components/Tabs"
 import Menu from "../components/Menu"
 
 const Layout: React.FC = () => {
@@ -13,7 +13,7 @@ const Layout: React.FC = () => {
                 <Menu />
                 <div className={styles.content}>
                     <h1 className={styles.title}>Иванов Иван</h1>
-                    <Tabs />
+                    <Outlet />
                 </div>
             </div>
             <Footer />
